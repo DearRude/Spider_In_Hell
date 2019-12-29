4391e1434ae18a176251efaf70b9f0f9dd893799
Ebrahim <enejati757@gmail.com>
2019-12-30 00:35:18 +0330

    Add getMatrix function

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#define MAXKEYWORD 15
 #include <stdio.h>
 
 int main() {
@@ -7,22 +6,11 @@ int main() {
 }
 
 int fgetMatrix(int adMatrix){
-    char matrixKeyword[] = "matrix"; //matrix keyword in map file
-    char nodeKeyword[] = "nodes"; //node keyword in map file
-    char keyword[MAXKEYWORD];
     // Reads Node counts and Matrix from file.
     FILE *map;
-    if (map = fopen("/src/map.txt", "r+") != NULL){
+    if (map = fopen("graphData", "r+") != NULL){
         while (!feof(map)){
-            if (fscanf(map, "%s", &keyword) == nodeKeyword){
-                // if it's node line
-                int nodeCounts;
-                fscanf(map, "%d", &nodeCounts);
-            }
-            if (fscanf(map,"%s", keyword ) == matrixKeyword){
-                // if it's matrix line
-                for (int line=0; line<nodeCounts) //could'nt do it!
-            }
+
         }
     }
 
