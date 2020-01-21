a04b71d02b05b0159b51d98a92e95e46981e4702
dearrude <enejati@mail.um.ac.ir>
2020-01-21 22:18:29 +0330

    Remove unfunctional data from master
    Signed-off-by: Ebrahim <enejati757@gmail.com>

--- main.c
+++ main.c
@@ -4,15 +4,3 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
-
-int fgetMatrix(int adMatrix){
-    // Reads Node counts and Matrix from file.
-    FILE *map;
-    if (map = fopen("graphData", "r+") != NULL){
-        while (!feof(map)){
-
-        }
-    }
-
-
-}
